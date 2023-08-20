8b5d240baf06d37a3eed780db95192baa9c2b582
root <root@SilverFangMkXI.localdomain>
2023-08-20 12:04:39 +0900

    操作パネルの見直しとDSWの値取得方法の見直し

--- tools/EcuEducationController.ps1
+++ tools/EcuEducationController.ps1
@@ -3,6 +3,7 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 [Application]::EnableVisualStyles()
+[void][reflection.assembly]::LoadWithPartialName("Microsoft.VisualBasic")
 
 #文字コードをUTF-8
 chcp 65001
@@ -38,27 +39,44 @@ $SelectInstance = "modelcar"
 # フォームを先に作成する
 $ControlFrame = New-Object Form
 $ControlFrame.Text = "Controller for ECU Education"
-# $ControlFrame.Text = $Args[2]
 
-#テキストボックスの数を決定する変数（2未満にはならず、最大5）
-$Formcnt = 4
-# $Formcnt = [int]$Args[1]
-
-#ラベルの名称配列 文字は「,」区切りで受ける
-$Tlab = @('One','Two','Three','Four')
-# $Tlab = $Args[3].Split(",")
-
-#ラベルの初期高さを変数へ格納
-$Hlab = 120
-
-#テキストボックスの初期高さを変数へ格納
-$Htxt = 140
-
-#ボタンの高さを計算して変数へ格納
+#ボタンの高さ
 $Hbtn = 20
 
 #フォームのサイズを決定する
 $ControlFrame.Size = "400, 280"
+
+# ネットワーク選択
+$NetworkSelectBtn = New-Object System.Windows.Forms.CheckBox
+$NetworkSelectBtn.Text = "1 ECU"
+$NetworkSelectBtn.Font = New-Object Drawing.Font("Meiryo UI",8)
+$NetworkSelectBtn.Size = "80, 37"
+$NetworkSelectBtn.Location = "20, " + [string]$Hbtn
+$NetworkSelectBtn.TextAlign = "MiddleCenter"
+$NetworkSelectBtn.Appearance = "Button"
+$NetworkSelectBtn.FlatStyle = "System"
+$NetworkSelectBtn.Checked = $False
+$NetworkSelectBtn.Enabled = $True
+
+$global:TargetMemFolder = "/root/modelcar/swc-application/1ecu/ecu_cbo"
+
+# ボタンイベント
+$NetworkSelectBtn_Click = {
+    if ( $NetworkSelectBtn.Checked -eq $False )
+    {
+        $NetworkSelectBtn.Text = "1 ECU"
+        $global:TargetMemFolder = "/root/modelcar/swc-application/1ecu/ecu_cbo"
+    }
+    else
+    {
+        $NetworkSelectBtn.Text = "2 ECUs"
+        $global:TargetMemFolder = "/root/modelcar/swc-application/1ecu/ecu_x"
+    }
+}
+$NetworkSelectBtn.Add_Click($NetworkSelectBtn_Click)
+
+#ボタンの要素をフォームへ加える
+$ControlFrame.Controls.Add($NetworkSelectBtn)
 
 # ボタン
 $DswBtn = New-Object System.Windows.Forms.CheckBox
@@ -70,8 +88,6 @@ $DswBtn.Location = "220, " + [string]$Hbtn
 $DswBtn.TextAlign = "MiddleCenter"
 $DswBtn.Appearance = "Button"
 $DswBtn.FlatStyle = "System"
-
-$global:toggle = 0
 
 # ボタンイベント
 $DswBtn_Click = {
@@ -85,19 +101,19 @@ $DswBtn_Click = {
         $DswBtn.Text = "DipSW ON"
         $Bin = 0
     }
-    $CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 seek=512 count=1 conv=notrunc 2>/dev/null"
+    $CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=" + $global:TargetMemFolder + "/mmap5.bin bs=1 seek=2 count=1 conv=notrunc 2>/dev/null"
     #Write-Host $CMD
     wsl docker exec $SelectInstance bash -c $CMD
 }
 $DswBtn.Add_Click($DswBtn_Click)
 
-$Bin = 128
-$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 seek=512 count=1 conv=notrunc 2>/dev/null"
+$Bin = 128  #デフォルトはOFF
+$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=" + $global:TargetMemFolder + "/mmap5.bin bs=1 seek=2 count=1 conv=notrunc 2>/dev/null"
 #Write-Host $CMD
 wsl docker exec $SelectInstance bash -c $CMD
 
 $Bin = 7
-$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 seek=513 count=1 conv=notrunc 2>/dev/null"
+$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=" + $global:TargetMemFolder + "/mmap5.bin bs=1 seek=2 count=1 conv=notrunc 2>/dev/null"
 #Write-Host $CMD
 wsl docker exec $SelectInstance bash -c $CMD
 
@@ -127,49 +143,19 @@ $PswBtn_Click = {
         $PswBtn.Text = "PushSW ON"  
         $Bin = 0
     }
-    $CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 seek=544 count=1 conv=notrunc 2>/dev/null"
+    $CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=" + $global:TargetMemFolder + "/mmap1.bin bs=1 seek=544 count=1 conv=notrunc 2>/dev/null"
     #Write-Host $CMD
     wsl docker exec $SelectInstance bash -c $CMD
 }
 $PswBtn.Add_Click($PswBtn_Click)
 
-$Bin = 3
-$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 seek=544 count=1 conv=notrunc 2>/dev/null"
+$Bin = 3    #デフォルトはOFF
+$CMD = "echo -en '\x" + $Bin.ToString("X2") + "' | dd of=" + $global:TargetMemFolder + "/mmap1.bin bs=1 seek=544 count=1 conv=notrunc 2>/dev/null"
 #Write-Host $CMD
 wsl docker exec $SelectInstance bash -c $CMD
 
 #ボタンの要素をフォームへ加える
 $ControlFrame.Controls.Add($PswBtn)
-
-# ネットワーク選択
-$NetworkSelectBtn = New-Object System.Windows.Forms.CheckBox
-$NetworkSelectBtn.Text = "1 ECU"
-$NetworkSelectBtn.Font = New-Object Drawing.Font("Meiryo UI",8)
-$NetworkSelectBtn.Size = "80, 37"
-$NetworkSelectBtn.Location = "20, " + [string]$Hbtn
-$NetworkSelectBtn.TextAlign = "MiddleCenter"
-$NetworkSelectBtn.Appearance = "Button"
-$NetworkSelectBtn.FlatStyle = "System"
-$NetworkSelectBtn.Checked = $False
-$NetworkSelectBtn.Enabled = $True
-
-# ボタンイベント
-$NetworkSelectBtn_Click = {
-    if ( $NetworkSelectBtn.Checked -eq $False )
-    {
-        $NetworkSelectBtn.Text = "1 ECU"
-#        $global:ControlTimer.Stop()
-    }
-    else
-    {
-        $NetworkSelectBtn.Text = "2 ECUs"
-#        $global:ControlTimer.Start()
-    }
-}
-$NetworkSelectBtn.Add_Click($NetworkSelectBtn_Click)
-
-#ボタンの要素をフォームへ加える
-$ControlFrame.Controls.Add($NetworkSelectBtn)
 
 # 接続ボタン
 $StartBtn = New-Object System.Windows.Forms.CheckBox
@@ -187,13 +173,13 @@ $StartBtn.Enabled = $True
 $StartBtn_CheckedChanged = {
     if ( $StartBtn.Checked -eq $False )
     {
-        $StartBtn.Text = "Stopping"
-#        $global:ControlTimer.Stop()
+        $StartBtn.Text = "Stopped"
+        $global:ControlTimer.Stop()
     }
     else
     {
         $StartBtn.Text = "Executing"
-#        $global:ControlTimer.Start()
+        $global:ControlTimer.Start()
     }
 }
 $StartBtn.Add_CheckedChanged($StartBtn_CheckedChanged)
@@ -205,19 +191,30 @@ $ControlFrame.Controls.Add($StartBtn)
 $LedStatus_1 = New-Object System.Windows.Forms.RadioButton
 $LedStatus_1.Location = New-Object System.Drawing.Point(20,20)
 $LedStatus_1.size = New-Object System.Drawing.Size(40,30)
+$LedStatus_1.Checked = $False
+$LedStatus_1.Enabled = $False
+$LedStatus_1.Text = ""
 
 $LedStatus_2 = New-Object System.Windows.Forms.RadioButton
 $LedStatus_2.Location = New-Object System.Drawing.Point(60,20)
 $LedStatus_2.size = New-Object System.Drawing.Size(40,30)
+$LedStatus_2.Checked = $False
+$LedStatus_2.Enabled = $False
+$LedStatus_2.Text = ""
 
 $LedStatus_3 = New-Object System.Windows.Forms.RadioButton
 $LedStatus_3.Location = New-Object System.Drawing.Point(100,20)
 $LedStatus_3.size = New-Object System.Drawing.Size(40,30)
-$LedStatus_3.Checked = $True
+$LedStatus_3.Checked = $False
+$LedStatus_3.Enabled = $False
+$LedStatus_3.Text = ""
 
 $LedStatus_4 = New-Object System.Windows.Forms.RadioButton
 $LedStatus_4.Location = New-Object System.Drawing.Point(140,20)
 $LedStatus_4.size = New-Object System.Drawing.Size(40,30)
+$LedStatus_4.Checked = $False
+$LedStatus_4.Enabled = $False
+$LedStatus_4.Text = ""
 
 # LedStatusグループ
 $LedStatusBox = New-Object System.Windows.Forms.GroupBox
@@ -233,11 +230,51 @@ $ControlFrame.Controls.Add($LedStatusBox)
 
 # タイマーを作成する
 $global:ControlTimer = New-Object System.Windows.Forms.Timer
-$global:ControlTimer.Interval = 2000
+$global:ControlTimer.Interval = 200    # 200 ms
 $global:ControlTimer.Stop()
 
 # タイマーイベント
 $ControlTimer_Tick = {
+    $memstatus = (wsl docker exec $SelectInstance bash -c "dd if=/root/modelcar/swc-application/1ecu/ecu_cbo/mmap1.bin bs=1 skip=32 count=1 2>/dev/null | od -x -N1 | head -1")
+    $memstatusLed = [Microsoft.VisualBasic.Strings]::Right( $memstatus, 2 )
+
+    switch($memstatusLed){
+        "ef"{
+            $LedStatus_1.Checked = $True
+            $LedStatus_2.Checked = $False
+            $LedStatus_3.Checked = $False
+            $LedStatus_4.Checked = $False
+            break
+        }
+        "df"{
+            $LedStatus_1.Checked = $False
+            $LedStatus_2.Checked = $True
+            $LedStatus_3.Checked = $False
+            $LedStatus_4.Checked = $False
+            break
+        }
+        "bf"{
+            $LedStatus_1.Checked = $False
+            $LedStatus_2.Checked = $False
+            $LedStatus_3.Checked = $True
+            $LedStatus_4.Checked = $False
+            break
+        }
+        "7f"{
+            $LedStatus_1.Checked = $False
+            $LedStatus_2.Checked = $False
+            $LedStatus_3.Checked = $False
+            $LedStatus_4.Checked = $True
+            break
+        }
+        default{
+            $LedStatus_1.Checked = $False
+            $LedStatus_2.Checked = $False
+            $LedStatus_3.Checked = $False
+            $LedStatus_4.Checked = $False
+            break
+        }
+    }
 }
 $global:ControlTimer.Add_Tick($ControlTimer_Tick)
 
